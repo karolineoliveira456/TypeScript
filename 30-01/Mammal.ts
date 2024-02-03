2279f8d37b42fdff9c57e79189259725ab0bb649
Karoline Oliveira <148974797+karolineoliveira456@users.noreply.github.com>
2024-02-03 16:24:28 -0300

    Update Mammal.ts

--- 30-01/Mammal.ts
+++ 30-01/Mammal.ts
@@ -1,1 +1,17 @@
-export class Mammal 
+class Mammal extends Animal {
+
+    private coat: string;
+
+    constructor(name: string, weight: number, coat: string) {
+        super(name, weight);
+        this.coat = coat;
+    }
+
+    public getCoat(): string {
+        return this.coat;
+    }
+
+    public breastfeed(): void {
+        console.log(`O mamifero esta amamentando...`);
+    }
+}
