871722847e3741bf2bff76c1ed88f04a34c3a28e
Karoline Oliveira <148974797+karolineoliveira456@users.noreply.github.com>
2024-02-05 11:24:01 -0300

    Update Mammal.js

--- 05-01. exZoo/Mammal.js
+++ 05-01. exZoo/Mammal.js
@@ -1,38 +1,23 @@
-"use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Mammal = void 0;
-var Animal_1 = require("./Animal");
-var Mammal = /** @class */ (function (_super) {
-    __extends(Mammal, _super);
-    function Mammal(name, weight, coat) {
-        var _this = _super.call(this, name, weight) || this;
-        _this.coat = coat;
-        return _this;
+import { Animal } from "./Animal";
+
+export class Mammal extends Animal {
+
+    private coat:string
+
+    constructor(name: string, weight: number, coat: string) {
+        super(name, weight);
+        this.coat = coat
     }
-    Mammal.prototype.getCoat = function () {
-        return this.coat;
-    };
-    Mammal.prototype.setCoat = function (coat) {
-        this.coat = coat;
-    };
-    Mammal.prototype.breastfeed = function () {
-        console.log("The mammal is breastfeeding...");
-    };
-    return Mammal;
-}(Animal_1.Animal));
-exports.Mammal = Mammal;
+
+    public getCoat():string{
+        return this.coat
+    }
+
+    public setCoat(coat:string): void{
+        this.coat = coat
+    }
+
+    public breastfeed(): void {
+        console.log(`The mammal is breastfeeding...`);
+    }
+}
